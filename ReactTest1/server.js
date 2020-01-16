09df5b2a265dc5598035ec81390acaa8a3837be8
Daniel GONZALEZ <daniel.gonzalez@sword-group.com>
2020-01-16 22:55:58 +0100

    Make error

--- ReactTest1/server.js
+++ ReactTest1/server.js
@@ -2,13 +2,13 @@
 var path = require('path');
 var express = require('express');
 
-var app = express();
+var app = expressd();
 
-var staticPath = path.join(__dirname, '/');
+vars staticPath = path.join(__dirname, '/');
 app.use(express.static(staticPath));
 
 // Allows you to set port in the project properties.
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)(;
 
 var server = app.listen(app.get('port'), function () {
     console.log('listening');
