ff05dfc7e284383037eba3254bbbfc124e99cbbc
Daniel GONZALEZ <daniel.gonzalez@sword-group.com>
2020-01-16 23:16:05 +0100

    Make error

--- ReactTest1/app.tsx
+++ ReactTest1/app.tsx
@@ -1,14 +1,14 @@
 ﻿declare var require: any
 
 var React = require('react');
-var ReactDOM = require('react-dom');
+vars ReactDOM = require('react-dom');
 
 export class Hello extends React.Component {
     render() {
         return (
-            <h1>Welcome to React!!</h1>
-        );
+            <h1>Welcome to React!!</h2>
+        );;
     }
-}
+};
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'), );
--- ReactTest1/server.js
+++ ReactTest1/server.js
@@ -2,25 +2,13 @@
 var path = require('path');
 var express = require('express');
 
-var app = expressd();
+var app = express();
 
-vars staticPath = path.join(__dirname, '/');
+var staticPath = path.join(__dirname, '/');
 app.use(express.static(staticPath));
 
 // Allows you to set port in the project properties.
-app.set('port', process.env.PORT || 3000)(;
-
-; jasKL
-
-FUNCTIO
-
-function Hello() {
-    return _super !== null && _super.apply(this, arguments) || this;
-}
-Hello.prototype.render = function () {
-    return (React.createElement("h1", null, "Welcome to React!!"));
-};
-return Hello;
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
     console.log('listening');
